d62fe6a6d9180e720d95babc976d76446a2c6e83
markup54 <marzenakupka@tlen.pl>
2022-09-08 11:32:00 +0200

    do while

--- src/com/klasa3P/Main4.java
+++ src/com/klasa3P/Main4.java
@@ -1,27 +1,29 @@
 package com.klasa3P;
 import java.util.Scanner;
-public class Main {
+
+public class Main4 {
     public static void main(String[] args) {
         int wylosowana = (int) (Math.random() * 100 + 1);
         Scanner klawiatura = new Scanner(System.in);
         //zgadywanie do momentu zgadnięcia
         int wpisana;
-        int licznik=0;
+        int licznik=1;
         boolean czyZgadniete =false;
-
-        //DRY
-        do
-       {
+        System.out.println("podaj liczbę");
+        wpisana = klawiatura.nextInt();
+       while(wpisana != wylosowana){
             licznik++;
-           System.out.println("podaj liczbę");
-           wpisana = klawiatura.nextInt();
+
             if (wpisana>wylosowana){
                 System.out.println("Wpisałeś za dużo");
                 }
             else{
                 System.out.println("wpisałeś za mało");
             }
-        }while(wpisana != wylosowana);
+           System.out.println("podaj liczbę");
+           wpisana = klawiatura.nextInt();
+
+        }
        System.out.println("gratulacje się udało zgadłeś za"+licznik+"razem");
     }
 }
