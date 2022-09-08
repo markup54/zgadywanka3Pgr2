cf63876671e5fa61a92143f8d6be2521e3be7471
markup54 <marzenakupka@tlen.pl>
2022-09-08 11:24:36 +0200

    while

--- src/com/klasa3P/Main.java
+++ src/com/klasa3P/Main.java
@@ -3,32 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int wylosowana = (int) (Math.random() * 100 + 1);
-        System.out.println(wylosowana);
         Scanner klawiatura = new Scanner(System.in);
-        //zgadywanie 10 razy
+        //zgadywanie do momentu zgadnięcia
         int wpisana;
-        int licznik=0;
+        int licznik=1;
         boolean czyZgadniete =false;
-        for(int i = 0 ;i<10;i++){
+        System.out.println("podaj liczbę");
+        wpisana = klawiatura.nextInt();
+       while(wpisana != wylosowana){
             licznik++;
-            System.out.println("podaj liczbę");
-            wpisana = klawiatura.nextInt();
-            if (wpisana == wylosowana) {
-                System.out.println("Gratulacje");
-                czyZgadniete =true;
-                break;
-            }
+
+            if (wpisana>wylosowana){
+                System.out.println("Wpisałeś za dużo");
+                }
             else{
-                if (wpisana>wylosowana){
-                    System.out.println("Wpisałeś za dużo");
-                }
-                else{
-                    System.out.println("wpisałeś za mało");
-                }
+                System.out.println("wpisałeś za mało");
             }
+           System.out.println("podaj liczbę");
+           wpisana = klawiatura.nextInt();
+
         }
-        if(!czyZgadniete){
-            System.out.println("słabiutko użyj algorytmu wyszukiwania binarnego");
-        }
+       System.out.println("gratulacje się udało zgadłeś za"+licznik+"razem");
     }
 }
---
+++ src/com/klasa3P/Main3.java
@@ -0,0 +1,35 @@
+package com.klasa3P;
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int wylosowana = (int) (Math.random() * 100 + 1);
+        System.out.println(wylosowana);
+        Scanner klawiatura = new Scanner(System.in);
+        //zgadywanie 10 razy
+        int wpisana;
+        int licznik=0;
+        boolean czyZgadniete =false;
+        for(int i = 0 ;i<10;i++){
+            licznik++;
+            System.out.println("podaj liczbę");
+            wpisana = klawiatura.nextInt();
+            if (wpisana == wylosowana) {
+                System.out.println("Gratulacje");
+                czyZgadniete =true;
+                break;
+            }
+            else{
+                if (wpisana>wylosowana){
+                    System.out.println("Wpisałeś za dużo");
+                }
+                else{
+                    System.out.println("wpisałeś za mało");
+                }
+            }
+        }
+        if(!czyZgadniete){
+            System.out.println("słabiutko użyj algorytmu wyszukiwania binarnego");
+        }
+    }
+}
